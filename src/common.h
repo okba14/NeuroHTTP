2bc68d276f95677333f57c6dcdf57b1eaef5d618
GUIAR OQBA <117593772+okba14@users.noreply.github.com>
2025-10-18 18:02:17 +0100

    Reorganize and update includes in common.h

--- src/common.h
+++ src/common.h
@@ -1,31 +1,38 @@
-// src/common.h
 #ifndef AIONIC_COMMON_H
 #define AIONIC_COMMON_H
 
 #define _POSIX_C_SOURCE 200809L
 #define _GNU_SOURCE
 
+// ===== C Standard Library =====
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <time.h>
-#include <pthread.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <signal.h>
-#include <sys/epoll.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/resource.h>
-#include <dlfcn.h>
-#include <dirent.h>
 #include <ctype.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <time.h>
+
+// ===== POSIX / System =====
+#include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+// ===== Networking =====
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+
+// ===== Dynamic Linking =====
+#include <dlfcn.h>
 
 #endif // AIONIC_COMMON_H
+
